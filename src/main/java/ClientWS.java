e76115d0ba7eeb135c93d7568c4de0416212de46
MaryamLemsyeh <lemsyeh17maryam@gmail.com>
2022-11-08 19:56:20 +0100

    first commit

---
+++ src/main/java/ClientWS.java
@@ -0,0 +1,15 @@
+import Package.BanqueService;
+import Package.BanqueWs;
+import Package.Compte;
+
+public class ClientWS {
+    public static void main(String[] args) {
+        BanqueService stub = new BanqueWs().getBanqueServicePort();
+        System.out.println(stub.convert(7600));
+        Compte cp = stub.getCompte(3);
+        System.out.println(cp.getCode());
+        System.out.println(cp.getSolde());
+
+
+    }
+}
